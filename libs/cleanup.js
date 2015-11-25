58c9fe1a5d5d29b6fb6b3a558e300fcfe548f4f5
Jonathan Dahan <jonathan@jonathan.is>
2015-11-25 02:49:00 -0500

    simplify cleanup code

--- libs/cleanup.js
+++ libs/cleanup.js
@@ -1,26 +1,18 @@
 const traverse = require('traverse')
 
 const cleanup = function (object) {
-  delete object['$']
-  traverse(object).forEach(function (e) {
+  traverse(object).map(function (e) {
+    if (this.circular) return this.remove()
     if (this.notLeaf && this.key != null && this.key.indexOf('List') > -1) {
       const prop = e[this.key.slice(0, -4)]
-      return prop instanceof Array ? prop : [prop]
+      return this.update((prop instanceof Array) ? prop : [prop])
     }
-    switch (e) {
-      case '':
-        return this.remove
-      case 'false':
-        return false
-      case 'true':
-        return true
-      default:
-        const i = parseInt(e)
-        if (!isNaN(i)){
-          return i
-        }
-    }
+    if (e === '') return this.delete()
+    if (e === 'false') return this.update(false)
+    if (e === 'true') return this.update(true)
+    if (!isNaN(parseInt(e))) return this.update(parseInt(e))
   })
+
   return object
 }
 
